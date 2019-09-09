ae4e5e0265bb82a179e009325dde76a7aaa358e1
Andriy Biletskyy <biletski.andriy@gmail.com>
2019-09-09 21:51:05 +0300

    Set a correct name for homework folders (HW20 to HW21)

--- FL11_HW20_homework_angular_1/homework/myApp/src/app/models/Source.ts
+++
@@ -1,5 +0,0 @@
-export class Source {
-    sourceId: number;
-    id: number;
-    sourceName: string;
-}
--- FL11_HW20_homework_angular_1/homework/myApp/src/app/pipes/selected-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SelectedPipePipe } from './selected-pipe.pipe';
-
-describe('SelectedPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SelectedPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
